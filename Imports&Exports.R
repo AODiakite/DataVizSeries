abd972549f86aa30883f33ef8a6200663c1bf0f9
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2022-04-24 22:18:47 +0000

    update

--- Imports&Exports.R
+++ Imports&Exports.R
@@ -1,20 +1,6 @@
----
-title: "This github repository's for a data visualization series"
-author: "ABDOUL OUDOUSS DIAKITE"
-date: "24 avril, 2022"
-output:
- html_document:
-    toc: yes
-    fig_caption: yes
-    keep_md: yes
----
-# Expots & Imports (Senegal)
-
-
-```r
 library(ggplot2)
 library(magrittr)
-library(readr)
+library(rvest)
 #Load data
 Data.SEN <- read_csv("SenegalIndicators.csv", 
                      skip = 4,show_col_types = F) %>% .[,-length(.)]
@@ -27,14 +13,12 @@ data$Date=rownames(data)
 names(data)[1]=c('Imports')
 data=merge(data1,data,by = 'Date')
 data = na.omit(data)
-```
 
 
-```r
 ggplot(data = data,aes(x= as.numeric(Date))) +
   geom_col(aes(y=Imports),fill = "#FF5733",size = 50) +
   geom_col(aes(y=Exports),fill  ="#0A7104",size =50,width = 0.7) +
-  labs(title = 'Senegal',subtitle = 'Imports & Exports (current US$)',caption = )+
+  labs(title = 'Senegal',subtitle = 'Imports & Exports (current US$)')+
   #Imports arrow
   annotate(geom = 'curve',
            x=1970, y = 2e+09,
@@ -85,7 +69,7 @@ ggplot(data = data,aes(x= as.numeric(Date))) +
                           "\n",
                           "Exports : ",data$Exports[data$Date=='2012'],
                           "\n"),
-           size = 2.1,hjust = "center",fill='#EBC894',fontface="bold")+
+           size = 3.1,hjust = "center",fill='#EBC894',fontface="bold")+
   #Max Exports arrow
   annotate(geom = 'curve',
            x=1970, y = 6e+09,
@@ -110,78 +94,5 @@ ggplot(data = data,aes(x= as.numeric(Date))) +
                           "\n",
                           "Exports : ",data$Exports[which.max(data$Exports)],
                           "\n"),
-           size = 2.1,hjust = "center",fill='#E5DC94',fontface="bold")+
+           size = 3.1,hjust = "center",fill='#E5DC94',fontface="bold")+
   ggthemes::theme_wsj()
-```
-
-![](README_files/figure-html/unnamed-chunk-2-1.png)<!-- -->
-
-# Covid 19 ( Senegal )
-
-
-```r
-# Load Covid Data
-library(dplyr)
-library(readr)
-SEN.covid<-  read_csv("https://covid19.who.int/WHO-COVID-19-global-data.csv",
-                      show_col_types = FALSE) %>% 
-     .[.$Country=='Senegal',] %>% filter(Date_reported >= '2020-03-02')
-```
-
-
-```r
-#ploting
-library(ggplot2)
-library(patchwork)
-p1= ggplot(SEN.covid,aes(x=Date_reported)) +
-  geom_col(aes(y = New_cases),color="darkblue") +
-  annotate(geom = 'col', 
-           x=SEN.covid[which.max(SEN.covid$New_cases),]$Date_reported,
-           y=max(SEN.covid$New_cases),colour ="red",size =1) + 
-  annotate(geom = 'curve',
-           x=lubridate::ymd('2020-07-28'), y = max(SEN.covid$New_cases)-500,
-           xend = SEN.covid[which.max(SEN.covid$New_cases)-2,]$Date_reported,
-           color ='#BB6203',
-           yend = max(SEN.covid$New_cases), 
-           curvature = -0.4, arrow = arrow(length = unit(4, "mm"))) +
-  annotate(geom = "label", x = lubridate::ymd('2020-07-28'), 
-           y = max(SEN.covid$New_cases)-600, 
-           label = paste0("Maximum number of cases : ",max(SEN.covid$New_cases),
-                          "\n","Date : ",
-                          SEN.covid[which.max(SEN.covid$New_cases)-1,]$Date_reported,
-                          "\n"),
-           size = 3.1,hjust = "center",fill='#FED5AA',fontface="bold")+
-  labs(title = 'Senegal',subtitle = 'Daily covid19 cases',
-       x='Date',y='New cases')+
-  ggthemes::theme_tufte() 
-
-p2 = ggplot(SEN.covid,aes(x=Date_reported)) +
-  geom_col(aes(y = New_deaths),color="#3F3E46") +
-  annotate(geom = 'col', 
-           x=SEN.covid[which.max(SEN.covid$New_deaths),]$Date_reported,
-           y=max(SEN.covid$New_deaths),colour ="#FF023F",size =1) +
-   annotate(geom = 'curve',
-           x=lubridate::ymd('2020-07-05'), y = max(SEN.covid$New_deaths)-10,
-           xend = SEN.covid[which.max(SEN.covid$New_deaths)-2,]$Date_reported,
-           color ='#041087',
-           yend = max(SEN.covid$New_deaths), 
-           curvature = -0.4, arrow = arrow(length = unit(4, "mm"))) +
-  annotate(geom = "label", x = lubridate::ymd('2020-07-07'), 
-           y = max(SEN.covid$New_deaths)-12, 
-           label = paste0("Maximum number of deaths : ",max(SEN.covid$New_deaths),
-                          "\n","Date : ",
-                          SEN.covid[which.max(SEN.covid$New_deaths)-1,]$Date_reported,
-                          "\n"),
-           fill='#B2B3D2', size = 3,hjust = "center",fontface="bold")+
-  labs(title = 'Senegal',subtitle = 'Daily deaths related to covid19',
-       x='Date',y='New deaths')+
-  ggthemes::theme_tufte()
-
-p1;p2
-```
-
-![](README_files/figure-html/unnamed-chunk-4-1.png)<!-- -->![](README_files/figure-html/unnamed-chunk-4-2.png)<!-- -->
-
-
-
-
